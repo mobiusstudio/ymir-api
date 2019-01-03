f6656032f8f933e8ec7f505abc453f240b896389
licetus <licetus0316@gmail.com>
2019-01-03 18:47:19 +0800

    rewrite task controller

--- src/controllers/Task.js
+++ src/controllers/Task.js
@@ -1,44 +1,40 @@
 const { Task } = global.models
 const { logger } = global
 
+const task = new Task()
+
 export const addTask = async (req, res) => {
   const data = req.swagger.params.body.value
   logger.trace('Add new task', data)
-  const object = await new Task(data).save()
+  const object = await task.add(data)
   return res.json(object)
 }
 
 export const getTaskList = async (req, res) => {
   const params = req.swagger.params.query
-  const t = new Task()
   logger.trace('Get task list', params)
-  const items = await t.getList(params)
-  const total = await t.getListCount(params)
-  return res.json({
-    total,
-    items,
-  })
+  const items = await task.from().select().do()
+  return res.json(items)
 }
 
 export const getTask = async (req, res) => {
   const id = req.swagger.params.taskId.value
   logger.trace('Get task', id)
-  const object = await new Task().get(id)
+  const object = await task.from().where`id = ${id}`.select().do()
   return res.json(object)
 }
 
 export const updateTask = async (req, res) => {
   const id = req.swagger.params.taskId.value
   const data = req.swagger.params.body.value
-  data.id = id
   logger.trace('Update task', data)
-  await new Task(data).save()
-  return res.sendStatus(200)
+  const code = await new Task(data).update(data, id)
+  return res.sendStatus(code)
 }
 
 export const deleteTask = async (req, res) => {
   const id = req.swagger.params.taskId.value
   logger.trace('Delete task', id)
-  await new Task().delete(id)
-  return res.sendStatus(200)
+  const code = await task.delete(id)
+  return res.sendStatus(code)
 }
