5d17fbac739f65f98b75c327ae8cf454dd2c8dba
licetus <licetus0316@gmail.com>
2019-01-04 22:45:01 +0800

    add paging definition in swagger, upge Task get list controller

--- src/controllers/Task.js
+++ src/controllers/Task.js
@@ -4,37 +4,65 @@ const { logger } = global
 const task = new Task()
 
 export const addTask = async (req, res) => {
-  const data = req.swagger.params.body.value
-  logger.trace('Add new task', data)
-  const object = await task.add(data)
-  return res.json(object)
+  try {
+    const data = req.swagger.params.body.value
+    logger.trace('Add new task', data)
+    const object = await task.add(data)
+    return res.json(object)
+  } catch (error) {
+    throw error
+  }
 }
 
 export const getTaskList = async (req, res) => {
-  const params = req.swagger.params.query
-  logger.trace('Get task list', params)
-  const items = await task.from().select().do()
-  return res.json(items)
+  try {
+    const { page, pagesize, next, paging } = req.swagger.params
+    const params = {
+      page: page.value,
+      pagesize: pagesize.value,
+      next: next.value,
+      nextKey: 'id',
+      filters: paging.value.filters,
+      orderBy: paging.value.orderBy,
+    }
+    logger.trace('Get task list', params)
+    const items = await task.from().do(params)
+    return res.json(items)
+  } catch (error) {
+    throw error
+  }
 }
 
 export const getTask = async (req, res) => {
-  const id = req.swagger.params.taskId.value
-  logger.trace('Get task', id)
-  const object = await task.from().where`id = ${id}`.select().do()
-  return res.json(object)
+  try {
+    const id = req.swagger.params.taskId.value
+    logger.trace('Get task', id)
+    const object = await task.from().where`id = ${id}`.select().do()
+    return res.json(object)
+  } catch (error) {
+    throw error
+  }
 }
 
 export const updateTask = async (req, res) => {
-  const id = req.swagger.params.taskId.value
-  const data = req.swagger.params.body.value
-  logger.trace('Update task', data)
-  const code = await new Task(data).update(data, id)
-  return res.sendStatus(code)
+  try {
+    const id = req.swagger.params.taskId.value
+    const data = req.swagger.params.body.value
+    logger.trace('Update task', data)
+    const result = await new Task(data).update(data, id)
+    return res.json(result)
+  } catch (error) {
+    throw error
+  }
 }
 
 export const deleteTask = async (req, res) => {
-  const id = req.swagger.params.taskId.value
-  logger.trace('Delete task', id)
-  const code = await task.delete(id)
-  return res.sendStatus(code)
+  try {
+    const id = req.swagger.params.taskId.value
+    logger.trace('Delete task', id)
+    const result = await task.delete(id)
+    return res.json(result)
+  } catch (error) {
+    throw error
+  }
 }
