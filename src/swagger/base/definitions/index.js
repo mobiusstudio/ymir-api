adb0448d77f1a597cf584aa77d90f9cebad3df0f
licetus <licetus0316@gmail.com>
2019-01-05 14:36:13 +0800

    crate swagger base config folders

---
+++ src/swagger/base/definitions/index.js
@@ -0,0 +1,18 @@
+import error from './error'
+import errorResponse from './errorResponse'
+import validationError from './validationError'
+import validationErrorResponse from './validationErrorResponse'
+import errorFieldDescription from './errorFieldDescription'
+import uploadToken from './uploadToken'
+
+const definitions = {
+  error,
+  errorResponse,
+  validationError,
+  validationErrorResponse,
+  errorFieldDescription,
+  uploadToken,
+
+}
+
+export default definitions
---
+++ src/swagger/base/paths/index.js
@@ -0,0 +1,10 @@
+import { assemblePath } from '../utils'
+import test from './test'
+
+const routes = {
+  test,
+}
+
+const basePath = ''
+
+export default assemblePath(routes, basePath)
